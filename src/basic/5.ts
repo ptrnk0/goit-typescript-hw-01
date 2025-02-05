09fea10df75f6085f66d46b714effe10537e346c
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-05 16:20:30 +0200

    add enum for days of the week and function to check if a day is a weekend

---
+++ src/basic/5.ts
@@ -0,0 +1,18 @@
+enum DayOfWeek {
+	Monday,
+	Tuesday,
+	Wednesday,
+	Thursday,
+	Friday,
+	Saturday,
+	Sunday,
+}
+
+const isWeekend = (day: DayOfWeek): boolean => {
+	if (day === DayOfWeek.Saturday || day === DayOfWeek.Sunday) {
+		return true;
+	}
+	return false;
+};
+
+isWeekend(DayOfWeek.Monday);
